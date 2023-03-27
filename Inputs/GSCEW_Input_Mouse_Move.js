434fa2e88e023f93830a36f6692c9a276eac0254
gulorecean <79078916+gulorecean@users.noreply.github.com>
2023-03-27 21:15:05 +0200

    Add files via upload

---
+++ Inputs/GSCEW_Input_Mouse_Move.js
@@ -0,0 +1,30 @@
+// make by Rémy Brandeau, All restrictives rights
+// MTCMC => Moteur (de rendu) de Transitions Complexes au Mouvement du Curseur 
+
+export default class Input_Mouse_Move {
+    constructor() {
+        this.details = {
+            name: "Input_Mouse_Move",
+            GSCEW_Version: 2,
+            urlProject: "https://github.com/",
+            dev: {
+                Input_Mouse_Move: [["Gulorecean", "https://brandeau.ovh/"]],
+            }
+        }
+        this.variables = {
+            event: undefined,
+            GSCEW: {
+                currentPosition: [0, 0]
+            }
+        }
+        this.GSCEW = undefined
+    }
+
+    start() {
+        window.addEventListener("mousemove", (e) => {
+            this.variables.event = e
+            this.variables.GSCEW.currentPosition = [e.clientX, e.clientY]
+            this.GSCEW.actualiseInput(this.details.name)
+        })
+    }
+}
---
+++ Inputs/GSCEW_Input_Scroll.js
@@ -0,0 +1,34 @@
+// make by Rémy Brandeau, All restrictives rights
+// MTCS => Moteur (de rendu) de Transitions Complexes au Scroll 
+
+export default class Input_Scroll {
+    constructor() {
+        this.details = {
+            name: "Input_Scroll",
+            GSCEW_Version: 2,
+            urlProject: "https://github.com/",
+            dev: {
+                Input_Scroll: [["Gulorecean", "https://brandeau.ovh/"]],
+            }
+        }
+        this.variables = {
+            event: undefined,
+            GSCEW: {
+                currentScroll: 0
+            }
+        }
+        this.GSCEW = undefined
+    }
+
+    start() {
+        document.addEventListener('wheel', (e) => {
+            this.variables.event = e
+            if(e.deltaY < 0 && this.variables.GSCEW.currentScroll != 0) {
+                this.variables.GSCEW.currentScroll -= 1
+            } else {
+                this.variables.GSCEW.currentScroll += 1
+            }
+            this.GSCEW.actualiseInput(this.details.name)
+        })
+    }
+}
